7fd447486626abc9580d62a4f11218b965bc07fa
jenkins <jenkins.chen@foxmail.com>
2020-04-17 12:05:53 +0800

    新增插入修改器

---
+++ src/main/java/com/team10/ojbattle/configuration/MyMetaObjectHandler.java
@@ -0,0 +1,43 @@
+package com.team10.ojbattle.configuration;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author: 陈健航
+ * @description: 插入/修改时间更新器
+ * @since: 2020/4/17 12:01
+ * @version: 1.0
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    /**
+     * 创建时自填装数据
+     *
+     * @param metaObject
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        //（实体类属性名字，数据类型，填充字段元数据）
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+
+    /**
+     * 更新时候自动填装数据
+     *
+     * @param metaObject
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+}
